6acd61fdb20e130fdb4c2732a6486dab68886eb3
Chris Peterson <cpeterson@mozilla.com>
2019-12-13 23:41:50 -0800

    Restrict search to BMO's non-Graveyard products

--- burndown/js/bugzilla.js
+++ burndown/js/bugzilla.js
@@ -137,6 +137,9 @@
         //url.push("&exclude_fields=alias,cc,cf_crash_signature,cf_qa_whiteboard,cf_user_story,classification,flags,groups,is_cc_accessible,is_confirmed,is_creator_accessible,platform,priority,qa_contact,see_also,severity,target_milestone,url,version");
         url.push("&include_fields=id,summary,is_open,creation_time,cf_last_resolved");
 
+        // Restrict search to BMO's non-Graveyard products.
+        url.push("&classification=Client%20Software&classification=Developer%20Infrastructure&classification=Components&classification=Server%20Software&classification=Other");
+
         url = url.join("");
         xhr(url, (error, response) => {
             if (error) {
